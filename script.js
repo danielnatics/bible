dcef3e203f9245ccb08b8f110dd1c21655e1746e
Engr Daniel <102671504+danielnatics@users.noreply.github.com>
2022-10-05 13:35:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const menuIcon = document.querySelector(".menu-icon");
 menuIcon.addEventListener("click", openMenu);
 function openMenu() {
   sidebar.classList.toggle("opensidebar");
-  menuIcon.toggle.src = "close.png";
+  
 }
