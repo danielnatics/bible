858ef5d6b5eaeb8874f715a617dc7d4416f63b65
Engr Daniel <102671504+danielnatics@users.noreply.github.com>
2022-10-05 20:27:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const sidebar = document.querySelector(".sidebar");
+const closeicon = document.querySelector(".menu-icon1");
 const menuIcon = document.querySelector(".menu-icon");
 menuIcon.addEventListener("click", openMenu);
 function openMenu() {
-  sidebar.classList.toggle("opensidebar");
+  sidebar.classList.add("opensidebar");
   
 }
+ menuIcon1.addEventListener("click", openMenu);
+function openMenu() {
+  sidebar.classList.remove("opensidebar");
+  
+}
+
